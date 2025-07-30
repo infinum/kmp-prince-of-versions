cda6ff0c2b819ba6b3291c17fb77ea16f9d96b40
Bruno Mikan <brunomikan@gmail.com>
2025-07-30 16:50:52 +0200

    Add kdoc to the storage interface

--- princeofversions/src/commonMain/kotlin/com/infinum/princeofversions/models/Storage.kt
+++ princeofversions/src/commonMain/kotlin/com/infinum/princeofversions/models/Storage.kt
@@ -1,7 +1,17 @@
 package com.infinum.princeofversions.models
 
+/**
+ * Represents a local device storage object that can be used to save and retrieve the application version.
+ */
 internal interface Storage {
+
+    /**
+     * Returns the last saved version of the application.
+     */
     fun getLastSavedVersion(): String?
 
+    /**
+     * Saves the version to the devices local storage
+     */
     fun saveVersion(version: String)
 }
