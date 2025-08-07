567ef9a71d5b7180cac172a326dd3578593ea378
Bruno Mikan <brunomikan@gmail.com>
2025-08-07 14:49:40 +0200

    Change comparators to default

---
+++ princeofversions/src/androidMain/kotlin/com/infinum/princeofversions/AndroidDefaultVersionComparator.kt
@@ -0,0 +1,7 @@
+package com.infinum.princeofversions
+
+internal class AndroidDefaultVersionComparator : VersionComparator<Int> {
+    override fun compare(firstVersion: Int, secondVersion: Int): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ princeofversions/src/iosMain/kotlin/com/infinum/princeofversions/IosDefaultVersionComparator.kt
@@ -0,0 +1,7 @@
+package com.infinum.princeofversions
+
+internal class IosDefaultVersionComparator: VersionComparator<String> {
+    override fun compare(firstVersion: String, secondVersion: String): Int {
+        TODO("Not yet implemented")
+    }
+}
