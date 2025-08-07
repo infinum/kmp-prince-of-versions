621b53af2da5930b5fa3aa5c45216c7360c41cc7
Bruno Mikan <brunomikan@gmail.com>
2025-08-07 15:26:21 +0200

    Add space before colon

--- princeofversions/src/iosMain/kotlin/com/infinum/princeofversions/IosDefaultVersionComparator.kt
+++ princeofversions/src/iosMain/kotlin/com/infinum/princeofversions/IosDefaultVersionComparator.kt
@@ -1,6 +1,6 @@
 package com.infinum.princeofversions
 
-internal class IosDefaultVersionComparator: VersionComparator<String> {
+internal class IosDefaultVersionComparator : VersionComparator<String> {
     override fun compare(firstVersion: String, secondVersion: String): Int {
         TODO("Not yet implemented")
     }
