8034c534753d6b90185d3daee0f5f316fe1c77a7
Bruno Mikan <brunomikan@gmail.com>
2025-08-29 16:56:43 +0200

    Change visibility

--- princeofversions/src/androidMain/kotlin/com/infinum/princeofversions/AndroidApplicationVersionProvider.kt
+++ princeofversions/src/androidMain/kotlin/com/infinum/princeofversions/AndroidApplicationVersionProvider.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 
-public class AndroidApplicationVersionProvider(context: Context) : ApplicationVersionProvider<Int> {
+internal class AndroidApplicationVersionProvider(context: Context) : ApplicationVersionProvider<Int> {
     private val version: Int = try {
         val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
         val longVersionCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
