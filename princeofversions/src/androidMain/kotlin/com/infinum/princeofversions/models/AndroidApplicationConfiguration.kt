25fcb518ce8118c9aa205a816cd0c068602f29cd
Bruno Mikan <brunomikan@gmail.com>
2025-09-11 14:25:25 +0200

    Fix kdoc

--- princeofversions/src/androidMain/kotlin/com/infinum/princeofversions/models/AndroidApplicationConfiguration.kt
+++ princeofversions/src/androidMain/kotlin/com/infinum/princeofversions/models/AndroidApplicationConfiguration.kt
@@ -12,10 +12,7 @@ internal class AndroidApplicationConfiguration(
 ) : ApplicationConfiguration<Int> {
 
     /**
-     * The application's version code, retrieved from the package manager.
-     * This implementation is backward-compatible and safely casts the version code to an Int.
-     * @throws IllegalStateException if the application's package name cannot be found,
-     * or if the version code is too large to be represented as an Int.
+     * The application's version code, retrieved from the version provider.
      */
     override val version: Int = versionProvider.getVersion()
 }
