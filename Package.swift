11aaeda6f23f5762750ebb5ccc4a5a70182e1a4d
Filip Stojanovski <filip.stojanovski2@infinum.hr>
2026-01-14 13:14:48 +0100

    Fix Package.swift URL to use correct release tag and download path. Remove the PrinceOfVersions.xcframework.zip. Itshould not be in the repository - it should only exist as a release asset on GitHub

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PrinceOfVersions",
-            url: "https://github.com/infinum/kmp-prince-of-versions/raw/0.1.0/PrinceOfVersions.xcframework.zip",
+            url: "https://github.com/infinum/kmp-prince-of-versions/releases/download/v0.1.0/PrinceOfVersions.xcframework.zip",
             checksum: "2d7b588610c023eff8a3f55e9e991574979aedb8c528ee46f7e9af5764e96362"
         )
     ]
